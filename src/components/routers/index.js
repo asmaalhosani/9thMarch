a519a50e33066b9a9dcd8ace3f9702a28f920b3a
asmaalhosani <124522569+asmaalhosani@users.noreply.github.com>
2023-05-16 10:29:02 +0400

    Add files via upload

---
+++ src/components/routers/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Login from "../auth/Login";
+import Login2 from "../auth/Login2";
+import Register from "../auth/Register";
+import Register2 from "../auth/Register2";
+import Landing from "../layouts/Landing";
+
+const Routers = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Landing></Landing>}></Route>
+        <Route path="/login" element={<Login2></Login2>}></Route>
+        <Route path="/register" element={<Register2></Register2>}></Route>
+      </Routes>
+    </>
+  );
+};
+
+export default Routers;
